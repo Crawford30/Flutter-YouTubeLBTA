f45937657e6c2c1e97838b69df367eab77d08766
joel <joelcrawford30@gmail.com>
2021-12-15 18:21:05 +0300

    Loading image

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class RealWorldApp extends StatefulWidget {
 
 class _RealWorldAppState extends State<RealWorldApp> {
   var _isLoading = true;
+  var videos;
 
   _fetchData() async {
     print("Attempting to print data from a network...");
@@ -29,8 +30,19 @@ class _RealWorldAppState extends State<RealWorldApp> {
 
     if(response.statusCode == 200){
       final map =   json.decode(response.body);
-        print(map["videos"]);
+       final videosJSON = (map["videos"]);
+
+       this.videos =  videosJSON;
+
+       // videosJSON.forEach((video) {
+       //   print(video["name"]);
+       // });
     }
+
+    setState(() {
+      _isLoading = false;
+      // this.videos =  videosJSON;
+    });
 
 
 
@@ -47,7 +59,7 @@ class _RealWorldAppState extends State<RealWorldApp> {
               print("Reloading...");
 
               setState(() {
-                _isLoading = false;
+                _isLoading = true;
               });
 
               _fetchData();
@@ -58,7 +70,32 @@ class _RealWorldAppState extends State<RealWorldApp> {
 
         body: new Center(
           child: _isLoading ? new CircularProgressIndicator() :
-          new Text("Finished Loading..."),
+          // new Text("Finished Loading..."),
+          new ListView.builder(
+            itemCount: this.videos != null ?  this.videos.length : 0,
+              itemBuilder: (context, i){
+
+              final video = this.videos[i];
+
+              return new Column(
+                children: [
+                  new Container(
+                    padding: new EdgeInsets.all(16.0),
+                    child: new Column(
+                      children: [
+                            new Image.network(video["imageUrl"]),
+                            new Text(video["name"]),
+
+                      ],
+                    ),
+                  ),
+
+                  new Divider()
+                ],
+              );
+              //return new Text("ROW: ${i}");
+              },
+          ),
         ),
       ),
 
@@ -69,7 +106,7 @@ class _RealWorldAppState extends State<RealWorldApp> {
   }
 }
 
-
+//https://stackoverflow.com/questions/54285172/how-to-solve-flutter-certificate-verify-failed-error-while-performing-a-post-req
 class MyHttpOverrides extends HttpOverrides{
   @override
   HttpClient createHttpClient(SecurityContext? context){
