5739b60b13ba05d510359b73761ce58c2cce6c3a
joel <joelcrawford30@gmail.com>
2021-12-15 19:37:03 +0300

    Detail Page

--- lib/main.dart
+++ lib/main.dart
@@ -77,30 +77,25 @@ class _RealWorldAppState extends State<RealWorldApp> {
               itemBuilder: (context, i){
 
               final video = this.videos[i];
+              
+              return new FlatButton(
+               padding: new EdgeInsets.all(0.0),
+                child: new VideoCell(video),
+                onPressed: (){
+                   print("Video cell tapped: ${i}");
+                   
+                   
+                   Navigator.push(context, 
+                   new MaterialPageRoute(builder: (context) => new DetailPage()),
+                  );
+                   
+                   //the context property is one on the list builder
+                },
+              );
 
 
 
-              // return new Column(
-              //   children: [
-              //     new Container(
-              //       padding: new EdgeInsets.all(16.0),
-              //       child: new Column(
-              //         crossAxisAlignment: CrossAxisAlignment.start,
-              //         children: [
-              //           new Image.network(video["imageUrl"]),
-              //           new Container(height: 8.0,),
-              //           new Text(video["name"],
-              //             style: new TextStyle(
-              //               fontSize: 16.0,
-              //               fontWeight: FontWeight.bold,
-              //             ),),
-              //         ],
-              //       ),
-              //     ),
-              //
-              //     new Divider()
-              //   ],
-              // );
+            
                return new VideoCell(video);
               //
               // //return new Text("ROW: ${i}");
@@ -165,4 +160,21 @@ class VideoCell extends StatelessWidget{
 
 
 
+}
+
+class DetailPage extends StatelessWidget {
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Detail Page"),
+      ),
+
+      body: new Center(
+        child: new Text("CENTERED"),
+      ),
+    );
+  }
 }
