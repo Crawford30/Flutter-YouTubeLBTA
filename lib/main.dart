6b65d396d3dd49ad0d5bb637e63b79b9640de90d
joel <joelcrawford30@gmail.com>
2021-12-15 17:31:55 +0300

    Solved http loading error

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(RealWorldApp());
+  HttpOverrides.global = MyHttpOverrides();
 }
 
 
@@ -14,6 +19,23 @@ class RealWorldApp extends StatefulWidget {
 }
 
 class _RealWorldAppState extends State<RealWorldApp> {
+  var _isLoading = true;
+
+  _fetchData() async {
+    print("Attempting to print data from a network...");
+
+    final url = "https://api.letsbuildthatapp.com/youtube/home_feed";
+    final response = await http.get(Uri.parse(url));
+
+    if(response.statusCode == 200){
+      final map =   json.decode(response.body);
+        print(map["videos"]);
+    }
+
+
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,13 +45,20 @@ class _RealWorldAppState extends State<RealWorldApp> {
           actions: <Widget>[
             new IconButton( icon: new Icon(Icons.refresh), onPressed:  (){
               print("Reloading...");
+
+              setState(() {
+                _isLoading = false;
+              });
+
+              _fetchData();
             })
           ],
 
         ),
 
         body: new Center(
-          child: CircularProgressIndicator(),
+          child: _isLoading ? new CircularProgressIndicator() :
+          new Text("Finished Loading..."),
         ),
       ),
 
@@ -37,5 +66,14 @@ class _RealWorldAppState extends State<RealWorldApp> {
     );
 
 
+  }
+}
+
+
+class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
   }
 }
